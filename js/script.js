d19df9f67c0de655338eb1e11123bfb7b23414e7
Vsevolod Surin <50403205+Vsevolods2@users.noreply.github.com>
2020-08-08 00:58:32 +0700

    Выполнение обязательных заданий в уроке 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-let money, income, addExpenses, deposit, misson, period;
+let money = 40000, 
+    income = 'freelance, primary job', 
+    addExpenses = 'Daily expenses, car, giftS', 
+    deposit = 'true',
+    mission = 100000, 
+    period = 3;
 
-alert("Hello, Government!");
+// alert("Hello, Government!");
 
-console.log("Hello, bots!");
+// console.log("Hello, bots!");
+
+console.log(typeof money, typeof income, typeof deposit);
+// строка выше: определяет тип данных переменных
+
+console.log(addExpenses.length); //определяем длину строки
+
+console.log('Период равен', period, 'месяцев,', 'цель заработать', mission, 'рублей');
+// строка выше: выводим переменную вместе с текстом
+
+console.log(addExpenses.toLowerCase(), addExpenses.split(', '));
+// строка выше: используем ловеркейс для перевода в нижний регистр;
+// сплит для разделения на массив строки
+
+console.log(budgetDay = money/30);
+// строка выше: объявили новую переменную, присвоили значение
